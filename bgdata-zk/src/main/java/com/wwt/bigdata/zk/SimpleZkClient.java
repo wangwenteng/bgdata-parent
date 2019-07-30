06647a3e58064d7e6ce97b8532f33deab414e177
wangwt <wangwt@llvision.com>
2019-07-30 17:54:49 +0800

    init for zk

---
+++ bgdata-zk/src/main/java/com/wwt/bigdata/zk/SimpleZkClient.java
@@ -0,0 +1,7 @@
+package com.wwt.bigdata.zk;
+
+public class SimpleZkClient {
+	
+	private static final String connectString="10.33.49.132:2181,10.33.49.133:2181,10.33.49.134:2181"; 
+	private static final int sessionTimeOut = 2000;
+}
