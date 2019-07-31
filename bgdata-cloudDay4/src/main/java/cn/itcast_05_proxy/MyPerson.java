0d60cc4366583a4e5289577124856a24677cc036
wangwt <wangwt@llvision.com>
2019-07-31 17:36:12 +0800

    cloudday4

---
+++ bgdata-cloudDay4/src/main/java/cn/itcast_05_proxy/MyPerson.java
@@ -0,0 +1,21 @@
+package cn.itcast_05_proxy;
+
+public class MyPerson implements PersonInterface{
+	
+	
+	public void doSomeThing(){
+		System.out.println("MyPerson is doing its thing.....");
+	}
+
+	
+	public void saySomeThing() {
+		System.out.println("MyPerson is saying its thing.....");
+		
+	}
+	
+	
+	private void xx(){
+		System.out.println("MyPerson is xx its thing.....");
+	}
+
+}
